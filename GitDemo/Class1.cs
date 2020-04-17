7fc1768dcd343c6a6b70904d024a5dbfba25fa3b
Slawomir Kierdelewicz <slawomir.kierdelewicz@cfteurope.pl>
2020-04-17 12:58:21 +0200

    7

---
+++ GitDemo/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDemo
+{
+    class Class1
+    {
+        int x;
+    }
+}
---
+++ GitDemo/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDemo
+{
+    class Class2
+    {
+        int y;
+    }
+}
