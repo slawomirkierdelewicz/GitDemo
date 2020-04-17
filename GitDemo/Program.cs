9541e37ce30d4905bbfc892f1f3f0e7440378165
Slawomir Kierdelewicz <slawomir.kierdelewicz@cfteurope.pl>
2020-04-17 11:15:24 +0200

    third change

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -10,7 +10,7 @@ namespace GitDemo
             Console.WriteLine("jagiellonia");
              Console.WriteLine("trufla");
             Console.WriteLine("Dex");
-            
+            Console.ReadKey();
         }
     }
 }
