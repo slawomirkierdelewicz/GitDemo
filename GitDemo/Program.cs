735f28b652797ab1ac4d33182ede32bd9481ad14
slawomirkierdelewicz <48598595+slawomirkierdelewicz@users.noreply.github.com>
2020-04-20 11:42:11 +0200

    Update Program.cs

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -13,6 +13,7 @@ namespace GitDemo
             Console.WriteLine("4");
              Console.WriteLine("5");
             Console.WriteLine("6");
+            Console.WriteLine("7");
             Console.ReadKey();
         }
     }
