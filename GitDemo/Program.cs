3ba853bb79d12bd47e9ecebb280f7fa2ad1bc7e6
Slawomir Kierdelewicz <slawomir.kierdelewicz@cfteurope.pl>
2020-04-20 13:38:57 +0200

    Revert "66"
    This reverts commit c492428e407fb87a96d7014496bd3b4377bdc283.

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -19,8 +19,6 @@ namespace GitDemo
             Console.WriteLine("10");
             Console.WriteLine("11");
             Console.WriteLine("12");
-            Console.WriteLine("13");
-
             Console.ReadKey();
 
 
