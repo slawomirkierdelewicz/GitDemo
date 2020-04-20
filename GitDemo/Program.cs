3df430790dd48f38ab0fc8851d1a68495dac2fe7
Slawomir Kierdelewicz <slawomir.kierdelewicz@cfteurope.pl>
2020-04-20 12:43:32 +0200

    ee

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -9,7 +9,10 @@ namespace GitDemo
             Console.WriteLine("Hello World!");
             Console.WriteLine("1");
             Console.WriteLine("2");
-       
+            Console.WriteLine("3");
+            Console.WriteLine("3"); 
+            Console.WriteLine("3");
+
             Console.ReadKey();
 
 
